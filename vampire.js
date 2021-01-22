b81f6df013b7c06be6f52ce317fe840630d2ab0f
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-22 21:10:10 +0000

    First few features

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let stepsRemoved = 0;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      stepsRemoved++;
+    }
+    return stepsRemoved;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -35,10 +49,18 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
 
+  closestCommonAncestor(vampire) {
+    if (this.creator === null) return this;
+
+    if (this.ancestor === vampire.ancestor) {
+      if (this.isMoreSeniorThan(vampire)) {
+        return this;
+      } else {
+        return vampire;
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
